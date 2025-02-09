1c55f741746bcb5cc3b5fa313eda30dffa8caf71
soma <swfh2367@gmail.com>
2025-02-09 16:22:02 +0900

    Refactor root layout to manage authentication state with Supabase

---
+++ app/(auth)/signupScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import { Signup } from "@/fetaure/auth/screen/Signup";
+
+export default function SignupScreen() {
+  return (
+    <View className="w-full p-4">
+      <Signup />
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,31 @@
-import { Stack } from "expo-router";
+// app/_layout.jsx
+import { SafeAreaView, Text } from "react-native";
+import { Slot } from "expo-router";
 
 import "../assets/styles/global.css";
 
+import { useEffect, useState } from "react";
+import { type Session } from "@supabase/supabase-js";
+
+import supabase from "@/lib/supabase";
+
 export default function RootLayout() {
-  return <Stack />;
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="items-left justify-top flex-1 p-4">
+      <Text>{session ? "ログイン済み" : "未ログイン"}</Text>
+      <Slot />
+    </SafeAreaView>
+  );
 }

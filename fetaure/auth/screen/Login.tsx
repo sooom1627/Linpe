c7a938d41684d5e9d50aa86ac2b14ab9c5d0c0a0
soma <swfh2367@gmail.com>
2025-02-09 15:26:12 +0900

    Enhance Login component with improved input attributes

--- fetaure/auth/screen/Login.tsx
+++ fetaure/auth/screen/Login.tsx
@@ -6,11 +6,23 @@ export const Login = () => {
       <Text className="text-2xl font-bold">Login</Text>
       <View className="flex flex-col gap-2">
         <Text className="text-lg font-bold">Email</Text>
-        <TextInput className="rounded-md border border-gray-300 p-2" />
+        <TextInput
+          className="rounded-md border border-gray-300 p-2"
+          autoCapitalize="none"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoComplete="email"
+        />
       </View>
       <View className="flex flex-col gap-2">
         <Text className="text-lg font-bold">Password</Text>
-        <TextInput className="rounded-md border border-gray-300 p-2" />
+        <TextInput
+          className="rounded-md border border-gray-300 p-2"
+          autoCapitalize="none"
+          textContentType="password"
+          keyboardType="ascii-capable"
+          autoComplete="password"
+        />
       </View>
       <TouchableOpacity className="items-center rounded-md bg-blue-500 p-2">
         <Text className="text-lg font-bold text-white">Login</Text>

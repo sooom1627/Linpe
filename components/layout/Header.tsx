90fe376d6bef40c677b2203fab1cfa5df9824ea1
soma <swfh2367@gmail.com>
2025-02-09 20:21:53 +0900

    fix desgin

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
     <View className="items-left justify-left h-16 flex-row border-b border-zinc-200 px-6 py-4">
       <View className="flex-row items-center justify-center gap-4">
         <View className="h-10 w-10 rounded-full bg-gray-500"></View>
-        <Text className="font-medium text-2xl leading-none">
+        <Text className="font-medium text-lg">
           Hi, {user?.username ?? "Guest"}.
         </Text>
       </View>

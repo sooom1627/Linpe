1800b4a07bfb67d155988b291e167a17c9916ccd
soma <swfh2367@gmail.com>
2025-02-09 13:43:55 +0900

    Add Husky and lint-staged for pre-commit hooks

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Hello World</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+});

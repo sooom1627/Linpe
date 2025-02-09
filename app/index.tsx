208c4ccce52e628cb9fdaffabc500bd7aa3fbc78
soma <swfh2367@gmail.com>
2025-02-09 13:51:39 +0900

    change text

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Hello World, Linpe</Text>
     </View>
   );
 }

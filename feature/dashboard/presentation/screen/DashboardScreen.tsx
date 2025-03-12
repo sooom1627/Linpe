be55410eca46d482f68ad96c694263d8aff19b1e
soma <swfh2367@gmail.com>
2025-03-13 07:38:57 +0900

    ダッシュボードコンポーネントをリファクタリングし、DashboardScreenを導入しました。これにより、ダッシュボードの表示が改善され、コードの可読性が向上しました。

---
+++ feature/dashboard/presentation/screen/DashboardScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export const DashboardScreen = () => {
+  return (
+    <View className="flex items-center justify-center gap-10 py-5">
+      <Text>Dashboard</Text>
+    </View>
+  );
+};

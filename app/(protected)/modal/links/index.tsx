794ec5f59b4d24b1aa6b0712ef8d6fc7159c039b
soma <swfh2367@gmail.com>
2025-03-18 23:35:30 +0900

    index.tsxファイルにおいて、カスタムヘッダーをLinksHeaderコンポーネントに置き換え、不要な戻るボタンのロジックを削除しました。これにより、コードの簡素化と可読性の向上が図られました。

--- app/(protected)/modal/links/index.tsx
+++ app/(protected)/modal/links/index.tsx
@@ -1,27 +1,13 @@
-import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { useRouter } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
-import { ChevronLeftIcon } from "@/components/icons/ChevronLeftIcon";
+import { LinksHeader } from "@/components/navigation/custom-header/LinksHeader";
 import { ThemedText } from "@/components/text/ThemedText";
 
 export default function LinksModal() {
-  const router = useRouter();
-
-  const handleBackPress = () => {
-    router.back();
-  };
-
   return (
     <View className="flex-1" style={styles.container}>
       {/* カスタムヘッダー部分 */}
-      <View className="h-16 flex-row items-center justify-between border-b border-zinc-200 bg-white px-4">
-        <View className="flex-row items-center justify-center gap-4">
-          <TouchableOpacity onPress={handleBackPress}>
-            <ChevronLeftIcon />
-          </TouchableOpacity>
-          <ThemedText text="Links" variant="h4" weight="semibold" />
-        </View>
-      </View>
+      <LinksHeader />
 
       {/* コンテンツ部分 */}
       <View className="flex-1 bg-white p-4">

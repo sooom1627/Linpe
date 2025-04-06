9858668654d844ebeb09653dea659cf5826a1aee
soma <swfh2367@gmail.com>
2025-04-06 14:30:27 +0900

    app.config.jsにSupabaseのURLと匿名キーを追加し、app.jsonのextraオブジェクトにrouterとeasの設定を追加しました。これにより、Supabaseの設定が一元管理され、プロジェクトの構成が改善されました。

--- app.config.js
+++ app.config.js
@@ -4,6 +4,7 @@ export default ({ config }) => {
   return {
     ...config,
     extra: {
+      ...config.extra,
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     },
